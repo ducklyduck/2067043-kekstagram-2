0828c8b66dd59f9cd21cac1dc3bd18902a9458f1
Nikita Ivanov <nivanov475@gmail.com>
2023-05-09 12:08:14 +0500

    fix: deleted unnecessary const and function

--- js/big-picture.js
+++ js/big-picture.js
@@ -2,10 +2,6 @@ import {isEscapeKey} from './util.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const commentList = bigPicture.querySelector('.social__comments');
-const commentsListFragment = document.createDocumentFragment();
-const commentTemplate = document.querySelector('#social__comment')
-  .content
-  .querySelector('.social__comment');
 
 const closeBigPictureElement= document.querySelector('.big-picture__cancel');
 
@@ -20,41 +16,6 @@ const onCloseBigPictureClick = (evt) => {
   evt.preventDefault();
   closeBigPicture();
 };
-
-// const showCommentList = (comments) => {
-//   comments.forEach((comment) => {
-//     const commentElement = commentTemplate.cloneNode(true);
-//     commentElement.querySelector('.social-picture').src = comment.avatar;
-//     commentElement.querySelector('.social__text').textContent = comment.message;
-
-//     commentsListFragment.appendChild(commentElement);
-//   });
-
-//   commentList.appendChild(commentsListFragment);
-// };
-
-// function openBigPicture (photo) {
-//   console.log('click');
-//   const img = bigPicture.querySelector('.big-picture__img').lastElementChild;
-
-//   img.src = photo.url;
-//   img.setAttribute('alt', 'Фото пользователя');
-
-//   bigPicture.querySelector('.social__caption').textContent = photo.description;
-//   bigPicture.querySelector('.likes-count').textContent = photo.likes;
-//   bigPicture.querySelector('.comments-count').textContent = photo.comments.length.toString();
-
-//   //Счетчик комментариев и кнопка "Загрузить больше" временно скрыты
-//   bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-//   bigPicture.querySelector('.comments-loader').classList.add('hidden');
-
-//   closeBigPictureElement.addEventListener('click', onCloseBigPictureClick);
-//   document.addEventListener('keydown', onBigPictureEscKeyDown);
-
-//   showCommentList(photo.comments);
-//   bigPicture.classList.remove('hidden');
-//   document.body.classList.add('modal-open');
-// }
 
 const openBigPicture = (pictureData) => {
   bigPicture.querySelector('.big-picture__img').querySelector('img').src = pictureData.url;
